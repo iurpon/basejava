36b2a57abdca4cf4ffc6df374501778fcc9b092b
cladendas <rvdagi05@yandex.ru>
2018-02-27 00:18:37 +0300

    создал класс MainReflection

---
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.lang.reflect.Field;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException {
+        Resume r = new Resume();
+        System.out.println(r);
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        field.get(r);
+        System.out.println(r);
+
+    }
+}
