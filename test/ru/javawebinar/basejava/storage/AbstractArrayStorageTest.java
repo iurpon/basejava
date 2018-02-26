21aa5517f09cac886fc669f33addcd2286563587
cladendas <rvdagi05@yandex.ru>
2018-02-26 22:13:55 +0300

    добавил в тест-класс AbstractArrayStorageTest метод с аннотацией @Before

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -1,10 +1,26 @@
 package ru.javawebinar.basejava.storage;
 
+import org.junit.Before;
 import org.junit.Test;
+import ru.javawebinar.basejava.model.Resume;
 
 import static org.junit.Assert.*;
 
 public class AbstractArrayStorageTest {
+
+    private Storage storage = new ArrayStorage();
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
+
     @Test
     public void size() throws Exception {
     }
