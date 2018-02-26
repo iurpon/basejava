c9ac31509696e80a6a0f27d2223670bd698ce332
cladendas <rvdagi05@yandex.ru>
2018-02-26 21:34:05 +0300

    создал тест класс для AbstractArrayStorage

---
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbstractArrayStorageTest {
+    @Test
+    public void size() throws Exception {
+    }
+
+    @Test
+    public void clear() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void save() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+}
