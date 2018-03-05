f44a9e07aceb481fa362655beaa21fd744b17779
cladendas <rvdagi05@yandex.ru>
2018-03-05 22:59:54 +0300

    HW06. добавил поле fullName в класс Resume, создал тест-классы AllStorageTest и AbstractArrayStorageTest (для тестирования массивов)

--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -5,22 +5,10 @@ import org.junit.Test;
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
-public class ArrayStorageTest extends AbstractStorageTest {
+public class ArrayStorageTest extends AbstractArrayStorageTest {
 
     public ArrayStorageTest() {
         super(new ArrayStorage());
-    }
-
-    @Test(expected = StorageException.class)
-    public void saveOverflow() throws Exception {
-        try {
-            for (int i = 4; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
-                storage.save(new Resume());
-            }
-        } catch (StorageException e) {
-            Assert.fail();
-        }
-        storage.save(new Resume());
     }
 
 }
