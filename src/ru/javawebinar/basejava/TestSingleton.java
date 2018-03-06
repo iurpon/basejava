87eb314501aa98dea0ebd6c4e32415c14af19499
cladendas <rvdagi05@yandex.ru>
2018-03-06 23:04:05 +0300

    Lesson 07. Осваивал синглетон

---
+++ src/ru/javawebinar/basejava/TestSingleton.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.basejava;
+
+public class TestSingleton {
+    private static TestSingleton instance;
+
+    public static TestSingleton getInstance() {
+        if (instance == null) {
+            instance = new TestSingleton();
+        }
+        return instance;
+    }
+
+    private TestSingleton() {
+    }
+
+    public static void main(String[] args) {
+        getInstance().toString();
+    }
+}
