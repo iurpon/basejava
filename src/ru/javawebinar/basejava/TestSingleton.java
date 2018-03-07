e3e7230572331a83b304fde275f45171c2cf984f
cladendas <rvdagi05@yandex.ru>
2018-03-07 16:31:11 +0300

    Lesson 07. Осваивал enum. Создали класс enum в директории model, добавили в него геттер для перечислений, а потом через цикл вывели их в консоль

--- src/ru/javawebinar/basejava/TestSingleton.java
+++ src/ru/javawebinar/basejava/TestSingleton.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.basejava;
 
+import ru.javawebinar.basejava.model.SectionType;
+
 public class TestSingleton {
     private static TestSingleton instance;
 
@@ -18,6 +20,9 @@ public class TestSingleton {
         Singleton instance = Singleton.valueOf("INSTANCE");
         System.out.println(instance.name());
         System.out.println(instance.ordinal());
+        for (SectionType type : SectionType.values()) {
+            System.out.println(type.getTitle());
+        }
     }
 
     public enum Singleton {
