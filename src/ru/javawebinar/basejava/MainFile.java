70aa7c5ee0aaaa96fd573e46ca8b40dd5918617c
cladendas <rvdagi05@yandex.ru>
2018-03-16 20:59:06 +0300

    HW09. Красивый вывод катологов

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -13,7 +13,7 @@ public class MainFile {
         } catch (IOException e) {
             throw new RuntimeException("Erroe", e);
         }
-        File dir = new File("./src/ru/javawebinar/basejava");
+        File dir = new File("./src/ru/javawebinar/");
         System.out.println(dir.isDirectory());
         String[] list = dir.list();
         if (list != null) {
@@ -28,18 +28,18 @@ public class MainFile {
             throw new RuntimeException(e);
         }
 
-        printDirectoryDeeply(dir);
+        printDirectoryDeeply(dir, "");
     }
 
-    public static void printDirectoryDeeply(File dir) {
+    public static void printDirectoryDeeply(File dir, String offset) {
         File[] files = dir.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    System.out.println(offset + "File: " + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    printDirectoryDeeply(file);
+                    System.out.println(offset + "Directory: " + file.getName());
+                    printDirectoryDeeply(file, offset + "  ");
                 }
             }
         }
